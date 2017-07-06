24f05d8fbffadbc2db1f97acaf2443c6cc2ece43
slava <slava@localhost.localdomain>
2017-07-06 19:26:25 +0600

    06/07/2017

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "https://free-talents.herokuapp.com/"}
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_options = { from: 'slava.nichkov@gmail.com' }
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
@@ -96,6 +97,7 @@ Rails.application.configure do
       password:             'Vyacheslav2406',
       authentication:       'plain',
       enable_starttls_auto: true  }
+  config.action_mailer.raise_delivery_errors = true
 
 end
 
