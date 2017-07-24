8d49da7915a819fe6b5f7d2cd030eae682096488
slava <slava@localhost.localdomain>
2017-07-24 14:44:55 +0600

    24/07/2017 JobsGalore.eu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with:  :render_404
-  rescue_from ActionController::RoutingError do |exception|
-          puts "_______________________Зашли"
-          render  status: 404
-  end
+  rescue_from ActionController::RoutingError, with:  :render_404
+  rescue_from CanCan::AccessDenied, with:  :render_404
   include ApplicationHelper
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -19,18 +17,8 @@ class ApplicationController < ActionController::Base
   @current_ability ||=Ability.new(current_client, params)
   end
 
-  rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden , content_type: ' text/html ' }
-      format.html { redirect_to "/404", notice: exception.message }
-      format.js { head :forbidden , content_type: ' text/html ' }
-      end
-  end
-
-
-
   def render_404
-    redirect_to '/'
+    render  status: 404
   end
 
 
