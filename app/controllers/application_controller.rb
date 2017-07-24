bc15d663168e48a9c9b8ead7cdc9b83d9f7750e8
slava <slava@localhost.localdomain>
2017-07-24 13:50:00 +0600

    24/07/2017 JobsGalore.eu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, :with => {render_404}
-  rescue_from ActionController::RoutingError, :with => {render_404}
+  rescue_from ActionController::RoutingError do |exception|
+    logger.error 'Routing error occurred'
+    render plain: '404 Not found', status: 404
+  end
+
   include ApplicationHelper
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
