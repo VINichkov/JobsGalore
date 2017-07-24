1f835b8cc864da98ab74042ae65544069d39fa42
slava <slava@localhost.localdomain>
2017-07-24 13:34:17 +0600

    24/07/2017 JobsGalore.eu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-  rescue_from ActionController::RoutingError, :with => :render_404
+  rescue_from ActiveRecord::RecordNotFound, :with => {render_404}
+  rescue_from ActionController::RoutingError, :with => {render_404}
   include ApplicationHelper
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
