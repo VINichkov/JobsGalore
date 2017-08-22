5a3d9f5bdfacc2d6e83725c573869d1508cb0d91
slava <slava@localhost.localdomain>
2017-08-22 17:19:09 +0600

    22.08.2017

--- app/models/client.rb
+++ app/models/client.rb
@@ -26,4 +26,8 @@ class Client < ApplicationRecord
     self.responsible ||= false
   end
 
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
 end
