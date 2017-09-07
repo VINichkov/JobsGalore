2b4c143066e29ba86e3c0ea79bb51e3c52441ee5
Nichkov Vyacheslav <v.nichkov@hotmail.com>
2017-09-07 10:16:14 +0300

    '07.09.2017'

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 # Configure dkim globally (see above)
 Dkim::domain      = 'jobsgalore.eu  '
 Dkim::selector    = 'email'
 Dkim::private_key = open('private.pem').read
 
 # This will sign all ActionMailer deliveries
-=======
-# Configure dkim globally (see above)
-Dkim::domain      = 'jobsgalore.eu  '
-Dkim::selector    = 'email'
-Dkim::private_key = open('private.pem').read
-
-# This will sign all ActionMailer deliveries
->>>>>>> 0066c0178741d550022581a86e9d3c71709682d0
-ActionMailer::Base.register_interceptor(Dkim::Interceptor)
+ActionMailer::Base.register_intergit ceptor(Dkim::Interceptor)
