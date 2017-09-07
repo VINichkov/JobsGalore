c5b8e98b9363d9b8d19170fbd44bbbdf47403d0b
Nichkov Vyacheslav <v.nichkov@hotmail.com>
2017-09-07 11:15:21 +0300

    '07.09.2017'

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -4,4 +4,4 @@ Dkim::selector    = 'email'
 Dkim::private_key = open('private.pem').read
 
 # This will sign all ActionMailer deliveries
-ActionMailer::Base.register_intergitceptor(Dkim::Interceptor)
+ActionMailer::Base.register_interceptor(Dkim::Interceptor)
