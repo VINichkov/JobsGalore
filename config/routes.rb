75bdb9317202429a4524bea939ad891872167e66
slava <slava@localhost.localdomain>
2017-10-10 16:51:19 +0600

    10.10.2017

--- config/routes.rb
+++ config/routes.rb
@@ -100,7 +100,7 @@ Rails.application.routes.draw do
   resources :responsibles
   resources :sizes
   resources :industries
-  resources :properts
+  resources :properts, only:[:index, :new, :create, :edit, :update, :destroy]
   resources :payments, only:[:show,:index]
 
 
