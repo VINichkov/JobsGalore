23898fbb94d339983723b2215d7d655b714e160e
Nichkov Vyacheslav <v.nichkov@hotmail.com>
2017-11-07 14:31:22 +0300

    07.11.2017

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,14 @@ module ApplicationHelper
       puts "____________________Error: #{$!}"
     end
   end
+
+  def title(text)
+    content_for :title, text
+  end
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+  def yield_meta_tag(tag, default_text='')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
 end
