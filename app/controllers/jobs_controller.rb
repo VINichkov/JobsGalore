b850db23888b5e529028b2f210c45fc5b73473cb
Nichkov Vyacheslav <v.nichkov@hotmail.com>
2017-11-22 15:18:20 +0300

    22.11.2017

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -76,9 +76,9 @@ class JobsController < ApplicationController
   end
 
   def admin_index
-    @not_id = Industryjob.select(:job_id).all.to_a
+    @not_id = Industryjob.all.map {|t| t.job_id}
     puts @not_id
-    @jobs = Job.where("id not in ?",@not_id).includes(:location,:company, :client).order(:close).paginate(page: params[:page], per_page:21)
+    @jobs = Job.where("id not in (?)",@not_id).includes(:location,:company, :client).order(:close).paginate(page: params[:page], per_page:21)
   end
 
   # GET /jobs/1
