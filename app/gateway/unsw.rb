ef599d59a60cd5813337712e0a3bda5f9fd86923
Nichkov Vyacheslav <v.nichkov@hotmail.com>
2017-12-05 07:43:43 +0300

    05.12.2017

--- app/gateway/unsw.rb
+++ app/gateway/unsw.rb
@@ -39,7 +39,7 @@ class Unsw < Adapter
             description += "<p><strong>Job Family:</strong> #{hash["Job Family"]}</p>"
             description += "<hr>"
             description += job.to_s
-            hash[:description] = Markitdown.from_nokogiri(Nokogiri::HTML(description.gsub("<br>"," ").gsub("<h1>","<h4>").gsub("<h2>","<h4>").gsub("<h3>","<h4>".gsub("</h1>","</h4>")).gsub("</h2>","</h4>").gsub("</h3>","</h4>").squish.gsub("> <","><")))
+            hash[:description] = Markitdown.from_nokogiri(Nokogiri::HTML(description.gsub("<br>"," ").gsub("h1","h4").gsub("h2","h4").gsub("h3","h4").squish.gsub("> <","><")))
             jobs.push ({  title: hash[:title],
                           close: hash["Close Date"],
                           fulltime: hash[:fulltime],
