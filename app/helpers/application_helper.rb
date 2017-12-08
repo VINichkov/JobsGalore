63f67a715b66ebe3441eb055f95e8666ab97b1e4
Nichkov Vyacheslav <v.nichkov@hotmail.com>
2017-12-08 13:01:46 +0300

    07.12.2017

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,10 +12,10 @@ module ApplicationHelper
 
   def markdown_to_keywords (arg)
     keys = markdown_to_text(arg, 300).split(' ').map do |key|
-      key.delete('?!@#;\'\",/\\|%^&*-_=+~1234567890.(){}[]')
-      key = nil unless key&.count>3
+      key.delete!('?!#;\'\",\\|%^&*-_=+~1234567890(){}[]')
+      key = '' unless key&.count>3
     end
-    keys.compact.join(', ')
+    keys.compact!.join(', ')
   end
 
   def markdown_to_text (arg, truncate=nil)
