457cc086720a5a4220d780f1ee5f7ac3feac0f3e
Nichkov Vyacheslav <v.nichkov@hotmail.com>
2017-12-08 12:05:19 +0300

    07.12.2017

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
   def yield_meta_tag(tag, default_text='')
     content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
   end
+
+  def markdown_to_keywords (arg)
+    markdown_to_text (arg).truncate(300).split(' ').join(', ')
+  end
+
+  def markdown_to_text (arg)
+    Nokogiri::HTML(RDiscount.new(arg).to_html).text.squish
+  end
 end
