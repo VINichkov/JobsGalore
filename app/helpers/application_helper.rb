fafb40e6531fc5578af30318c1aaa517a85b546e
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-05-03 09:36:11 +0200

    03.05.2018

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,9 @@ module ApplicationHelper
     content_for :title, text
   end
   def meta_tag(tag, text)
-    puts "meta_#{tag}"
     content_for :"meta_#{tag}", text
   end
   def yield_meta_tag(tag, default_text='')
-    puts "meta_#{tag} = #{default_text}"
     content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
   end
 
@@ -24,7 +22,6 @@ module ApplicationHelper
   end
 
   def meta_head(arg={})
-    puts arg
     title arg[:title]
     meta_tag "description",   arg[:description]
     meta_tag "keywords", arg[:keywords]
