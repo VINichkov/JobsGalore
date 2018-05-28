5bbdd54e17a4b11445830e9b57b89c456ac0788b
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-05-28 13:41:59 +0200

    28.05.2018

--- app/interactors/category_view.rb
+++ app/interactors/category_view.rb
@@ -14,6 +14,7 @@ class CategoryView
     context.objs = category.company.order(:name).paginate(page: @page, per_page:21).includes(:industry,:location).decorate
     context.type = Objects::COMPANIES
     context.name = {name:'Companies by', industry: category.name}
+    context.key = "Company, Companies, Industry, employment, Jobs Galore, Australia, Resumes, Resume, Galore, Jobsgalore, #{category.name} ,#{category.name} companies, #{category.name} company"
     true
   end
 
@@ -22,6 +23,7 @@ class CategoryView
     context.objs = category.job.order(created_at: :desc).paginate(page: @page, per_page:25).includes(:company,:location).decorate
     context.type = Objects::JOBS
     context.name = {name:'Jobs by', industry: category.name}
+    context.key = "Job, Jobs, Industry, employment, Jobs Galore, Australia, Resumes, Resume, Galore, Jobsgalore, #{category.name} ,jobs #{category.name},jobs in #{category.name},job #{category.name},job in #{category.name},work #{category.name},work in #{category.name}, #{category.name} job, #{category.name} jobs, #{category.name} work"
     true
   end
 
@@ -30,6 +32,7 @@ class CategoryView
     context.objs = category.resume.order(updated_at: :desc).paginate(page: @page, per_page:25).includes(:location).decorate
     context.type = Objects::RESUMES
     context.name = {name:'Resumes by', industry: category.name}
+    context.key = "Resume, Resumes, CV, CVs,Talents, Talent, Industry, employment, Jobs Galore, Australia, Resumes, Resume, Galore, Jobsgalore, #{category.name} ,resume #{category.name},resumes in #{category.name},CV #{category.name},CVs in #{category.name},Talent #{category.name},Talents in #{category.name}, #{category.name} resume, #{category.name} CV, #{category.name} Talent"
     true
   end
 end
