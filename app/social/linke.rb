3bff402d71f7e209c9bc41b44321eb48313dea94
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-07-02 17:55:51 +0600

    02.07.2018

---
+++ app/social/linke.rb
@@ -0,0 +1,16 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Linke <LinkedIn
+      option :name, 'linke'
+
+      def raw_info
+        Rails.logger.debug('Зашли  в класс')
+        @raw_info ||= access_token.get("/v1/people/~:(#{option_fields.join(',')})?format=json").parsed
+      end
+    end
+  end
+end
+
+OmniAuth.config.add_camelization 'linke', 'Linke'
