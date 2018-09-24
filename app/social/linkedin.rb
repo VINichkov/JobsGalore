af5b4c7439e73019db886b8872743ab738de8edc
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-09-24 10:16:16 +0600

    24.09.2018

--- app/social/linkedin.rb
+++ app/social/linkedin.rb
@@ -5,6 +5,8 @@ module OmniAuth
     class LinkedIn1 < OmniAuth::Strategies::OAuth2
 
 
+      option :scope, 'r_basicprofile r_emailaddress r_fullprofile'
+
       def access_token
         ::OAuth2::AccessToken.new(client, oauth2_access_token.token, {
             :mode => :query,
