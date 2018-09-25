5709c027de2c3927b42e2d0e25dc7e723eb5270d
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-09-25 16:57:27 +0600

    21.09.2019

--- app/social/linkedin.rb
+++ app/social/linkedin.rb
@@ -5,6 +5,15 @@ module OmniAuth
     class LinkedIn < OmniAuth::Strategies::OAuth2
       option :scope, 'r_basicprofile r_emailaddress rw_company_admin w_share'
       option :fields, ['id', 'email-address', 'first-name', 'last-name', 'headline', 'location', 'industry', 'picture-url', 'public-profile-url', 'summary', 'positions']
+
+      def raw_info
+        Rails.logger.debug("_______________________________________________")
+        Rails.logger.debug(access_token.to_s)
+        Rails.logger.debug("/v1/people/~:(#{option_fields.join(',')})?format=json")
+        Rails.logger.debug("_______________________________________________")
+        @raw_info ||= access_token.get("/v1/people/~:(#{option_fields.join(',')})?format=json").parsed
+      end
+
     end
   end
 end
