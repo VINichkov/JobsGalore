d7c60998392cc3bfea48340a9b68ce74e2bca8e0
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-10-05 16:25:59 +0600

    05/09/2019

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
    #      :if => Proc.new { |rack_env| rack_env['SERVER_NAME'] != 'www.jobsgalore.eu' }
   #end
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  #config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
