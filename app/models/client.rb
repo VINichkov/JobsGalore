3f1618770bffadfea91d0023e09706fd9bf8a63f
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-10-22 17:56:44 +0600

    22.10.2018

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,6 +9,8 @@ class Client < ApplicationRecord
 
   before_save :rename, :type
 
+
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: %i[linkedin]
   if Rails.env.production?
@@ -33,6 +35,41 @@ class Client < ApplicationRecord
     end
   end
 
+  def self.from_omniauth(auth)
+    Rails.logger.debug "Client::from_omniauth #{auth.to_json}"
+    local = Location.search((auth.info.location.name.delete("!.,:*&()'`\"’").split(" ").map {|t| t=t+":*"}).join("|")).first
+    client = where(provider: auth.provider, uid: auth.uid).or(where(email: auth.info.email)).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.firstname = auth.info.first_name
+      user.lastname = auth.info.last_name
+      user.token = auth.credentials.token
+      user.sources = auth.info.urls.public_profile
+      user.location = (local ? local : Location.default)
+      user.photo_url = auth.info.image # assuming the user model has an image
+      user.character=TypeOfClient::APPLICANT
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.skip_confirmation!
+    end
+    client.update(token: auth.credentials.token, sources: auth.info.urls.public_profile)
+    [client, Resume.new(LinkedInClient.new.linkedin_to_h(auth))]
+  end
+
+  def self.new_with_session(params, session)
+    Rails.logger.debug "new_with_session зашли"
+    super.tap do |user|
+      Rails.logger.debug "Создали новую сессию"
+      if data = session["devise.linkedin_data"] && session["devise.linkedin_data"]["extra"]["raw_info"]
+        Rails.logger.debug "Обновим все"
+        user.provider ||=auth.provider
+        user.uid ||=auth.uid
+        user.token = auth.credentials.token
+        user.sources ||= auth.info.urls.public_profile
+        user.photo_url ||= auth.info.image
+      end
+    end
+  end
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
@@ -56,5 +93,72 @@ class Client < ApplicationRecord
        true
      end
    end
+
+  def full_name
+    @full_name ||= self.firstname+' '+self.lastname
+  end
+
+  def to_short_h
+    {id:id, firstname:firstname,
+     lastname:lastname, email:email,
+     phone:phone, password:password,
+     photo_uid: photo_uid,
+     gender:gender,
+     location_id:location_id,
+     character:character,
+     company_id:company_id}
+  end
+
+  def resumes_for_apply
+    resume.select(:id, :title, :salary, :description, :location_id, :industry_id).all.limit(5).includes(:location,:industry).map  do |t|
+      {idr: t.id,
+       title: t.title,
+       location: t.location&.name,
+       industry: t.industry&.name,
+       salary: t.salary,
+       description: t.description }
+    end
+  end
+
+  def admin?
+    self.email == PropertsHelper::ADMIN
+  end
+
+  def resp?
+    character==TypeOfClient::EMPLOYER or character==TypeOfClient::EMPLOYEE
+  end
+
+  def employer?
+    character==TypeOfClient::EMPLOYER
+  end
+
+  def employee?
+    character==TypeOfClient::EMPLOYEE
+  end
+
+  def applicant?
+    character==TypeOfClient::APPLICANT
+  end
+
+  def change_type
+    self.employer? ? self.character=TypeOfClient::EMPLOYEE : self.character=TypeOfClient::EMPLOYER
+    self.save
+  end
+
+  def add_type(arg)
+    arg == TypeOfClient::APPLICANT ?  self.character = TypeOfClient::APPLICANT : self.character = TypeOfClient::EMPLOYER
+  end
+
+  def type
+    if self.character.nil?
+      self.character=TypeOfClient::APPLICANT
+    elsif character == 'on'
+      self.character = TypeOfClient::EMPLOYER
+    end
+  end
+
+  def linkedin?
+    sources ? true : false
+  end
 
 end
