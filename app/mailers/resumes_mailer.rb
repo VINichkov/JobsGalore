5c22cc2bc03ecd54ad79c72a42bfba51a4a28973
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-10-29 17:26:19 +0600

    29.10.2018

--- app/mailers/resumes_mailer.rb
+++ app/mailers/resumes_mailer.rb
@@ -1,12 +1,12 @@
 class ResumesMailer < ApplicationMailer
 
-
   def add_resume(resume)
     @resume = resume
     mail(to:@resume[:mail], subject: "Your resume was published on JobsGalore!")
   end
 
   def send_to_employer(resume, job, letter)
+    Rails.logger.debug("Q_____________________________________________________________________!")
     @resume = resume
     @job = job
     @letter = letter
