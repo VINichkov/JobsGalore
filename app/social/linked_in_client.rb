5a1b8f6b25ff185918d5e73ea83892125351f24d
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-10-29 17:13:47 +0600

    29.10.2018

--- app/social/linked_in_client.rb
+++ app/social/linked_in_client.rb
@@ -3,6 +3,7 @@ class LinkedInClient
   def linkedin_to_h(auth)
     if auth
       experience = auth&.extra&.raw_info&.positions[:values]&.last
+      summary = ''
       summary = auth&.extra&.raw_info&.summary.split("\n").compact.map{|t| "<p>#{t}</p>"}.join if auth.extra.raw_info.summary
       local = Location.search((auth.extra.raw_info.location.name.delete("!.,:*&()'`\"’").split(" ").map {|t| t=t+":*"}).join("|")).first
       if experience
