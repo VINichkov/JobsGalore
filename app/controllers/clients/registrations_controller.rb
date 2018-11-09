9efdfdfa291a96e5331ce4aeb66caafb819b906f
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-11-09 14:29:03 +0600

    09.11.2018

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -25,6 +25,7 @@ class Clients::RegistrationsController < Devise::RegistrationsController
      resource.save
      @client_wf.update_state(client:resource)
      if resource.persisted?
+       resource.skip_confirmation! if @client_wf.class == Redirect
        if resource.active_for_authentication?
          set_flash_message! :notice, :signed_up
          sign_up(resource_name, resource)
