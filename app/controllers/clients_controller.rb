466490e2b5ca070d6c62896ba3a5dc1c2b92ff1f
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-11-13 15:22:17 +0600

    13.11.2018

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -43,7 +43,7 @@ class ClientsController < ApplicationController
   def linkedin_resume_update
     if current_client
       if Rails.env.production?
-        @response = LinkedInClient.new.client.linkedin_to_h(client.get_profile(current_client.token))
+        @response = LinkedInClient.new.linkedin_to_h(client.get_profile(current_client.token))
       else
         @response = {:title=>"Administrator JobsGalore.eu",
                      :industry_id=>19,
