a25d3cd3f723067e7376e58de302985c9bddc1be
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-11-19 14:14:05 +0600

    19.11.2018

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -1,7 +1,7 @@
 # Configure dkim globally (see above)
 Dkim::domain      = 'jobsgalore.eu  '
 Dkim::selector    = 'email'
-Dkim::private_key = open(Rails.root.join('key/private.pem')).read
+Dkim::private_key = OpenSSL::PKey::RSA.new(ENV['DM_KIT_KEY'].gsub("\\n", "\n")) #open(Rails.root.join('key/private.pem')).read
 
 # This will sign all ActionMailer deliveries
 ActionMailer::Base.register_interceptor(Dkim::Interceptor)
