fd18e8d61d06ed044aa6503472c269218101e66d
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2018-11-20 17:43:12 +0600

    20.11.2018

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,41 +22,42 @@ set :repo_url, "git@github.com:VINichkov/JobsGalore.git"
 
 set :pty,             true
 set :use_sudo,        true
-#set :stage,           :production
-#set :deploy_via,      :remote_cache
-#set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-#set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-#set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-#set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-#set :puma_access_log, "#{release_path}/log/puma.error.log"
-#set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :user,             'ubuntu'
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 #set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-#set :puma_preload_app, true
-#set :puma_worker_timeout, nil
-#set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+ set :scm,           :git
+ set :branch,        :master
+ set :format,        :pretty
+ set :log_level,     :info
+ set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
- set :linked_files, %w{config.ru Procfile Rakefile}
- set :linked_dirs,  %w{bin log app config lib loc public tmp vendor}
+ #set :linked_files, %w{config/database.yml}
+ #set :linked_dirs,   %w{bin log tmp/pids tmp/cache tmp/sockets public/system}
 
-#namespace :puma do
-#  desc 'Create Directories for Puma Pids and Socket'
-#  task :make_dirs do
-#    on roles(:app) do
-#      execute "mkdir #{shared_path}/tmp/sockets -p"
-#      execute "mkdir #{shared_path}/tmp/pids -p"
-#    end
-#  end
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
 
-#  before :start, :make_dirs
-#end
+  before :start, :make_dirs
+end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
@@ -65,7 +66,7 @@ namespace :deploy do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
-        exit
+        #exit
       end
     end
   end
