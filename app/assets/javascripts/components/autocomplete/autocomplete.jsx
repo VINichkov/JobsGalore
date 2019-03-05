6abf26017d7ecde8282a770b8bb2236404dbd5d5
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-03-05 14:52:29 +0600

    test to dev

--- app/assets/javascripts/components/autocomplete/autocomplete.jsx
+++ app/assets/javascripts/components/autocomplete/autocomplete.jsx
@@ -1,4 +1,3 @@
-
 class Autocomplete extends React.Component{
     constructor(props) {
         super(props);
@@ -8,11 +7,29 @@ class Autocomplete extends React.Component{
                         locations: null,
                         defaultId: this.props.defaultId,
                         defaultName: this.props.defaultName,
-                        input:this.props.not_id ? false : true };
+                        input: !this.props.not_id };
+        this.value_to_array = function (arg) {
+            if (arg !== null){
+                return arg.map(function (e) {
+                    return e.name;
+                })
+            } else{
+                return null;
+            }
+        };
         this.handleInput = this.handleInput.bind(this);
     }
     componentDidMount(){
-        this.setState({autocomplete: document.querySelector('#' + this.props.id)});
+        let input = $('#' + this.props.id).first();
+        if (input !=null) {
+            input.typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1,
+                source:  '', //["rub", "rubber", "rubbing", "rubbish", "ruby", "rudder", "rude", "rudimentary", "rueful", "ruffle" ],
+            });
+            this.setState({autocomplete: input});
+        }
         if (this.state.input) {
             this.setState({input_id: document.querySelector('#input_get' + this.props.id)});
         }
@@ -20,16 +37,16 @@ class Autocomplete extends React.Component{
     handleInput(){
         let not_found = true;
         if (this.state.input && this.state.locations !==null){
-            for(var i=0; i< this.state.locations.length; i++){
-                if (this.state.locations[i].name ==this.state.autocomplete.value){
+            for(let i=0; i< this.state.locations.length; i++){
+                if (this.state.locations[i].name === this.state.autocomplete.value){
                     this.state.input_id.value =(this.state.locations[i].id);
                     not_found = false;
                     break;
                 }
             }
         }
-        if (not_found && this.state.autocomplete.value.length>0) {
-            this.handleSearchLocations(this.props.route+this.state.autocomplete.value+".json");
+        if (not_found && this.state.autocomplete.val().length>0) {
+            this.handleSearchLocations(this.props.route+this.state.autocomplete.val()+".json");
         }
 
     }
@@ -38,28 +55,23 @@ class Autocomplete extends React.Component{
         $.ajax({url:url,
             success: function (data) {
                 this.setState({locations:data});
+                if (this.state.autocomplete !==null && this.state.locations !== null)  {
+                    this.state.autocomplete.data('typeahead').source = this.state.locations;
+                }
             }.bind(this)});
     }
 
     render(){
         const ilStyle={display:'none'};
-        if ( this.state.locations !== null ) {
-            var locations = this.state.locations.map(function(location) {
-                return(
-                            <option key={'location_li'+this.state.id+location.id} data-id = {location.id} value={location.name} />);
-            }.bind(this));
-        }
         let input_id = null;
         if (this.state.input) {
-            input_id = <input ref={this.props.idRef} key={this.props.name + "_id]"} id={"input_get"+this.props.id} name={this.props.name + "_id]"} defaultValue = {this.state.defaultId} className={this.props.className} style = {ilStyle}/>;
+            input_id = <input ref={this.props.idRef} key={this.props.name + "_id]"} id={"input_get"+this.props.id} name={this.props.name + "_id]"} defaultValue = {this.state.defaultId} className={this.props.className+" typeahead"} style = {ilStyle}/>;
         }
         return(
             <div>
-                <input list={this.props.name}  ref={this.props.nameRef} key = {this.state.input ? this.props.name + "_name]" : this.props.name} name={this.state.input ? this.props.name + "_name]" : this.props.name} autoComplete = "off" className={this.props.className} onInput={this.handleInput}  defaultValue = {this.state.defaultName} placeholder={this.props.place_holder} type="text"  id={this.props.id} style={this.props.style}></input>
+                <input   ref={this.props.nameRef} key = {this.state.input ? this.props.name + "_name]" : this.props.name} name={this.state.input ? this.props.name + "_name]" : this.props.name} autoComplete = "off" className={this.props.className} onInput={this.handleInput}  defaultValue = {this.state.defaultName} placeholder={this.props.place_holder} type="text"  id={this.props.id} style={this.props.style}/>
                 {input_id}
-                <datalist id={this.props.name} >
-                    {locations}
-                </datalist>
+
             </div>
         );
     }
