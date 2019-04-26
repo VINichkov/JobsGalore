2c36bdf1e5e8b20b42400c5fb749a0db840fa406
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-04-26 12:26:15 +0600

    26.04.2019

--- app/models/admin/create_client_with_resume.rb
+++ app/models/admin/create_client_with_resume.rb
@@ -17,36 +17,41 @@ class Admin::CreateClientWithResume
   attribute :resume
 
   def save
-    Rails.logger.debug "phone = #{phone}, email = #{email}, (phone || email) = #{(phone || email)}"
+    flag = false
     self.password = phone.present? ? phone : email
-    client = Client.new(
-      email: email,
-      phone: phone,
-      password: password,
-      password_confirmation: password,
-      location_id: location_id
-    )
-    client.full_name = fullname
-    if client.save
+    client = Client.find_by_email(email)
+    if client.blank?
+      flag = true
+      client = Client.new(
+        email: email,
+        phone: phone,
+        password: password,
+        password_confirmation: password,
+        location_id: location_id
+      )
+      client.full_name = fullname
+      errors.merge!(client.errors) unless client.save
+    end
+    if errors.blank?
       self.resume = Resume.new(
         title: title,
         description: description,
-        location_id: location_id,
+        location_id: client.location_id,
         industry_id: industry,
         client: client
       )
       if resume.save
-        params = {
-          name: client.firstname,
-          email: email,
-          password: password
-        }
-        OtherMailer.the_resume_has_posted(email, "RE: #{subject}", params).deliver_later
+        if flag
+          params = {
+            name: client.firstname,
+            email: email,
+            password: password
+          }
+          OtherMailer.the_resume_has_posted(email, "RE: #{subject}", params).deliver_later
+        end
       else
         errors.merge!(resume.errors)
       end
-    else
-      errors.merge!(client.errors)
     end
     errors.blank?
   end
