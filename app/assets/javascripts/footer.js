84e1dea053f363719fcd46aecf1c8cae75c2f16f
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-07-11 15:46:24 +0600

    11.07.2019

---
+++ app/assets/javascripts/footer.js
@@ -0,0 +1,7 @@
+//= require jquery.min.js
+//= require jquery_ujs
+//= require bootstrap.min
+//= require bootstrap-datepicker
+//= require bootstrap-typeahead
+//= require social-share-button
+//= require jasny-bootstrap
