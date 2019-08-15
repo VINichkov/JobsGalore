fd2fa70e45e7f7356a4e02b94a52aec1b0046425
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-08-15 15:56:00 +0600

    15.08.2019

---
+++ lib/go/crawler.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+
+
+func main() {
+	start := time.Now()
+	//Crawl("https://golang.org/", 4, fetcher)
+	response, err := http.Get("https://jobsgalore.eu")
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", response.StatusCode, response.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	fmt.Println(doc.Attr("id"))
+	end := time.Now()
+
+	fmt.Println("Звакончили за %s",  end.Sub(start))
+} // fakeFetcher is Fetcher that returns canned results.
+
