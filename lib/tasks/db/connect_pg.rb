cf51b1114aeb5db2afd64bd2e352804b11efca39
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-09-16 16:17:47 +0600

    fix

--- lib/tasks/db/connect_pg.rb
+++ lib/tasks/db/connect_pg.rb
@@ -8,11 +8,14 @@ class ConnectPg
       config = YAML.load_file("#{Rails.root}/config/database.yml")[ENV['RAILS_ENV']]
       conf_app = YAML.load_file("#{Rails.root}/config/application.yml")[ENV['RAILS_ENV']]
       config['username'] = conf_app['MONGO_DATABASE_USER']
-      config['password'] = conf_app['MONGO_DATABASE_PASSWORD']
+      config['password'] = conf_app['MANGO_DATABASE_PASSWORD']
     else
       ENV['RAILS_ENV'] ||= 'development'
       config = YAML.load_file("#{Rails.root}/config/database.yml")[ENV['RAILS_ENV']]
     end
+    puts "config['database'] = #{config['database']}"
+    puts "config['database'] = #{config['username']}"
+    puts "config['database'] = #{config['password']}"
     @connect = PG.connect(
       dbname: config['database'],
       user: config['username'],
