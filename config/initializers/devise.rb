03a8da48a21df413683638773e7c5e8718ee4869
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-10-11 10:54:26 +0600

    experiment with linkedin

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,8 +250,14 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :linkedin, ENV["LINKEDIN_APP_ID"], ENV["LINKEDIN_APP_SECRET"], token_params: { parse: :json }
+  config.omniauth :linkedin,
+                  ENV["LINKEDIN_APP_ID"],
+                  ENV["LINKEDIN_APP_SECRET"],
+                  scope: [:r_emailaddress, :r_fullprofile],
+                  token_params: { parse: :json }
+
   config.omniauth :google_oauth2, ENV["GOOGLE_APP_ID"], ENV["GOOGLE_APP_SECRET"], {}
+
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
                   client_options: {
                       site: "https://graph.facebook.com/v3.2",
