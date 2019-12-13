1e98bb7288cf3f51ac61c1015cb4aae5a427585f
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-12-13 10:04:50 +0600

    fix bag in a view

--- app/helpers/location_helper.rb
+++ app/helpers/location_helper.rb
@@ -2,4 +2,5 @@ module LocationHelper
   def link_location(name = nil, location = nil, type= nil, html_options = nil, &block)
     link_to name , local_object_path(location.id,type.code), html_options, &block
   end
+
 end
