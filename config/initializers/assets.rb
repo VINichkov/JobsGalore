a544fac7735087b13543efacb982c73bac56d018
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2020-02-05 10:59:22 +0600

    propellerads in xs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
- Rails.application.config.assets.precompile += %w( footer.js footer.scss application_xs.scss )
+ Rails.application.config.assets.precompile += %w(application_xs.scss sw.js)
